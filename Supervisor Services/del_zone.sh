7f0b59f73b394194a822d637e0372b6c926ccea5
Burke Azbill <burkeazbill@users.noreply.github.com>
2025-07-01 15:38:02 -0400

    Remove invalid files

--- Supervisor Services/del_zone.sh
+++
@@ -1,2 +0,0 @@
-ses_auth=$(curl -k -X POST https://vc-mgmt-a.site-a.vcf.lab/api/session -u 'administrator@vsphere.local':'VMware123!VMware123!')
-curl -k -X DELETE https://vc-mgmt-a.site-a.vcf.lab/api/vcenter/consumption-domains/zones/zone-1 -H "vmware-api-session-id: $(echo ${ses_auth} | tr -d '"')" -H 'Content-Type: application/json' | jq
--- Supervisor Services/get_zone.sh
+++
@@ -1,2 +0,0 @@
-ses_auth=$(curl -k -X POST https://vc-mgmt-a.site-a.vcf.lab/api/session -u 'administrator@vsphere.local':'VMware123!VMware123!')
-curl -k -X GET https://vc-mgmt-a.site-a.vcf.lab/api/vcenter/consumption-domains/zones -H "vmware-api-session-id: $(echo ${ses_auth} | tr -d '"')" -H 'Content-Type: application/json' | jq
